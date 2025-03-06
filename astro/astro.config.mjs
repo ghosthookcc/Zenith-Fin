b00385cf53c4f5147e703af5ae1824ffa02391a7
ghosthookcc <kasperthornsten@gmail.com>
2025-03-06 11:14:37 +0100

    Implement .env file configuration

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig(
+{
+	env : 
+	{
+		schema : 
+		{
+			SOCIAL_SECURITY_NUMBER: envField.string({ context : "server", access : "secret" }),
+			DATA_ENCRYPTION_PASSWORD: envField.string({ context : "server", access : "secret" }),
+		}
+	}
+});
