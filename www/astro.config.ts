0f8dbe7697bebcdce6117ce234d767b1a8d1dd52
Kasper Anon <kasperthornsten@gmail.com>
2026-01-18 12:01:27 +0100

    Change running port for application

--- www/astro.config.ts
+++ www/astro.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
+  server: {
+      port: 4444,
+      host: true
+  }
 });
